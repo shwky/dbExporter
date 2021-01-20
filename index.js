2b88d4daced6c27f1213d8b7cfe5e066e2d19b55
shwky <shwky.fareed@gmail.com>
2021-01-20 19:19:52 +0200

    upload to s3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
+const mysqldump = require('mysqldump');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 const PORT = process.env.PORT
 
 //start my server on the port specified
@@ -9,18 +11,8 @@ app.listen(PORT, () => {
     console.log('Server Started ....');
 });
 
-
-const mysql = require('mysql');
-
-//connect to  db
-let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-})
-
-const mysqldump = require('mysqldump');
+let today = new Date().toISOString().slice(0, 10);
+const fileName = `bk-${today}.sql`;
 
 //dump the result to a file
 mysqldump({
@@ -30,5 +22,30 @@ mysqldump({
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
     },
-    dumpToFile: './dbfile2.sql',
+    dumpToFile: fileName,
+}).then( data => {
+    //console.log(data);
 });
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
+
+const uploadFile = () => {
+    fs.readFile('./' + fileName, (err, data) => {
+        if (err) throw err;
+        const params = {
+            Bucket: 'storkyappstatic', 
+            Key: 'dbbackups/'+fileName, 
+            Body: JSON.stringify(data, null, 2)
+        };
+        s3.upload(params, function (s3Err, data) {
+            if (s3Err) throw s3Err
+            console.log(`File uploaded successfully at ${data.Location}`)
+        });
+    });
+};
+
+uploadFile();
+
